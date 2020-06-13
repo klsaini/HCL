936eaadc2d2b0422c2b5bcb754f691eed42986d2
klsaini <41240776+klsaini@users.noreply.github.com>
2020-06-13 13:02:13 +0530

    Update BFS.java

--- BFS.java
+++ BFS.java
@@ -1,4 +1,25 @@
+/*
+Design a  binary tree and then print the breadth first order traversal of this tree. In breadth first order traversal, we visit the nodes of same height first then go to nodes of next height or level.
+     1
 
+      \
+
+       2
+
+        \
+
+         5
+
+        /  \
+
+       3    6
+
+        \
+
+         4 
+
+For the above tree, the level order traversal is 1 -> 2 -> 5 -> 3 -> 6 -> 4.
+*/
 import java.util.LinkedList;
 import java.util.Queue;
 
